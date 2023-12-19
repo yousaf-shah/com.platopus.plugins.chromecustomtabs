e69350b118375369ef01542e4a133f47da924ce1
yousaf-shah <59312216+yousaf-shah@users.noreply.github.com>
2023-12-19 04:51:33 +0000

    Improved example

--- Corona/main.lua
+++ Corona/main.lua
@@ -1,5 +1,17 @@
 local chromeCustomTabs = require "plugin.chromeCustomTabs"
 
+local myRectangle = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
+myRectangle:setFillColor( 0.5 )
+
+local log = display.newText{
+    text = "Tap anywhere to present chromeCustomTab.\n",
+    x = display.contentCenterX,
+    y = display.contentCenterY,
+    width = display.contentWidth,
+    fontSize = 10,
+    align = "center"
+}
+
 -- This event is dispatched to the global Runtime object
 -- by `didLoadMain:` in MyCoronaDelegate.mm
 local function delegateListener( event )
@@ -14,30 +26,43 @@ Runtime:addEventListener( "delegate", delegateListener )
 -- This event is dispatched to the following Lua function
 -- by PluginLibrary::show() in PluginLibrary.mm
 
-local function listener( event )
+function myTouchListener( event )
 
-	print( "Received event from chromeCustomTabs plugin (" .. event.name .. "): ", event.message )
+    if event.phase == "ended" then
+
+	    if chromeCustomTabs.supportsCustomTabs() then
+
+			local function listener( event )
+
+				log.text = log.text .. "\nAction received: " .. event.message
+				print( "Received event from chromeCustomTabs plugin (" .. event.name .. "): ", event.message )
+
+			end
+
+			chromeCustomTabs.initCustomTab( "https://solar2d.com/", listener )
+
+			chromeCustomTabs.warmupCustomTab()
+
+			print( "Init chromeCustomTabs plugin ...")
+
+			timer.performWithDelay( 4000, function()
+
+				chromeCustomTabs.showCustomTab()
+
+			end )
+
+		else
+
+
+			log.text = log.text .. "\nChrome Custom Tabs are not supported"
+			print( "Custom Tabs not supported on this Device")
+
+		end
+
+	end
 
 end
+Runtime:addEventListener( "touch", myTouchListener )
 
 
-if chromeCustomTabs.supportsCustomTabs() then
-
-	chromeCustomTabs.initCustomTab( "https://solar2d.com/", listener )
-
-	chromeCustomTabs.warmupCustomTab()
-
-	print( "Init chromeCustomTabs plugin ...")
-
-	timer.performWithDelay( 4000, function()
-
-		chromeCustomTabs.showCustomTab()
-
-	end )
-
-else
-
-	print( "Custom Tabs not supported on this Device")
-
-end
 
